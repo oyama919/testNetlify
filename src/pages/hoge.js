12c3da62c78ed183f0cbab160603012d34d62b96
oyama919 <oyama919@gmail.com>
2019-02-17 11:32:03 +0900

    add example page

---
+++ src/pages/hoge.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText="About Gatsby" />
+    <p>Such wow. Very hoge.</p>
+    <p>Such wow. Very hoge.</p>
+    <p>Such wow. Very hoge.</p>
+    <p>Such wow. Very hoge.</p>
+    <p>Such wow. Very hoge.</p>
+    <p>Such wow. Very hoge.</p>
+  </div>
+)
